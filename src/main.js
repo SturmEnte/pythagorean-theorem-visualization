603f26517cdff40924d976be7acbffe00e63aaf1
Jonas <sturmente@proton.me>
2023-03-23 18:32:42 +0100

    Finished basic rendering of the visualization

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementsByTagName("canvas")[0];
 const ctx = canvas.getContext("2d");
 
-resize();
-render();
+ctx.translate(0, canvas.height);
+ctx.scale(1, -1);
 
 let a = 1;
 let b = 1;
@@ -10,10 +10,46 @@ let b = 1;
 function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const unit = (path = new Path2D());
-    path.moveTo(75, 50);
-    path.lineTo(100, 75);
-    path.lineTo(100, 25);
+    let w = 2 * a + b;
+    let h = a + 2 * b;
+
+    let unit, xOff, yOff;
+
+    if (canvas.width < canvas.height) {
+        unit = canvas.width / w;
+        xOff = 0;
+        yOff = (canvas.height - canvas.width) / 2;
+    } else {
+        unit = canvas.height / h;
+        xOff = (canvas.width - canvas.height) / 2;
+        yOff = 0;
+    }
+
+    a = a * unit;
+    b = b * unit;
+
+    // Base Trigangle
+    let path = new Path2D();
+    path.moveTo(a + xOff, b + yOff);
+    path.lineTo(a + xOff, a + b + yOff);
+    path.lineTo(a + b + xOff, 2 * b + yOff);
+    ctx.fill(path);
+
+    // a²
+    ctx.fillStyle = "red";
+    ctx.fillRect(0 + xOff, b + yOff, a, a);
+
+    // b²
+    ctx.fillStyle = "blue";
+    ctx.fillRect(a + xOff, a + b + yOff, b, b);
+
+    // c²
+    ctx.fillStyle = "orange";
+    path = new Path2D();
+    path.moveTo(a + xOff, b + yOff);
+    path.lineTo(a + b + xOff, 0 + yOff);
+    path.lineTo(a + 2 * b + xOff, a + yOff);
+    path.lineTo(a + b + xOff, 2 * b + yOff);
     ctx.fill(path);
 }
 
@@ -26,3 +62,6 @@ new ResizeObserver(() => {
     resize();
     render();
 }).observe(canvas);
+
+resize();
+render();
