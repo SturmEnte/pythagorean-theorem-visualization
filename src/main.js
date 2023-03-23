9833f6a01d073594e06910f89895dc9a523592d7
Jonas <sturmente@proton.me>
2023-03-23 17:34:54 +0100

    Fixed the resize function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 const canvas = document.getElementsByTagName("canvas")[0];
 const ctx = canvas.getContext("2d");
 
+resize();
+render();
+
+let a = 1;
+let b = 1;
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const unit = (path = new Path2D());
+    path.moveTo(75, 50);
+    path.lineTo(100, 75);
+    path.lineTo(100, 25);
+    ctx.fill(path);
+}
+
 function resize() {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 }
 
-canvas.onresize = resize;
-resize();
+new ResizeObserver(() => {
+    resize();
+    render();
+}).observe(canvas);
